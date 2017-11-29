d458d605b1b434169034c8ac5e2e24b9ae725b3d
thymen heersmink <jt.heersmink@student.alfa-college.nl>
2017-11-29 11:42:03 +0100

    update

---
+++ Stenden/portfolio.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Stenden</title>
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+ <body>
+  <div id="header">
+    <div id="headercontent">
+    <a href=""><img src="img/logo_print.png" alt="Homepage"></a>
+
+
+    <ul id="mainmenu">
+      <li><a href="#">Portfolio Aanpassen</a></li>
+
+      <li><a href="#">Zoek portfolio</a></li>
+    </ul>
+
+    <ul id="loggen">
+      <li><a href="#">Log uit</a></li>
+    </ul>
+    </div>
+</div>
+
+
+<div id="content">
+    <div id="article">
+      <div id="profile">
+        <div id="pic">
+          <a href=""><img src="img/empty_profile.png" alt="Profile"></a>
+        </div>
+
+        <div id="info">
+          <p>Naam:</p>
+          <p>Klas:</p>
+          <p>Leerlingnummer:</p>
+          <p>Over mij:</p>
+        </div>
+      </div>
+    </div>
+
+
+    <div id="article">
+      <h1 style="text-align: center; width: 100%; border-bottom: 1px solid black">Insert title</h1>
+      
+    </div>
+  </div>
